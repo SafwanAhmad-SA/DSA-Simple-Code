e89ac3b121734ee41f8799c4fe02d2c9d9cd59c9
Safwan Ahmad <safwanahmadaim@gmail.com>
2026-06-14 21:02:27 +0600

    Update mergeSort.cpp

--- Merge Sort/mergeSort.cpp
+++ Merge Sort/mergeSort.cpp
@@ -1,36 +1,36 @@
 #include<iostream>
 using namespace std;
 
-void marge(int left[], int leftSize, int right[], int rightSize, int result[]){
+void merge(int left[], int leftSize, int right[], int rightSize, int result[]) {
     int i = 0, j = 0, k = 0;
 
-    while (i < leftSize && j < rightSize){
-        if(left[i] < right[j]){
+    while (i < leftSize && j < rightSize) {
+        if (left[i] < right[j]) {
             result[k] = left[i];
             i++;
         }
-        else{
+        else {
             result[k] = right[j];
             j++;
         }
         k++;
     }
 
-    while(i < leftSize){
+    while (i < leftSize) {
         result[k] = left[i];
         i++;
         k++;
     }
 
-    while(j < rightSize){
+    while (j < rightSize) {
         result[k] = right[j];
         j++;
         k++;
     }
 }
 
-void margeSort(int arr[], int size){
-    if(size <= 1){
+void mergeSort(int arr[], int size) {
+    if (size <= 1) {
         return;
     }
 
@@ -39,21 +39,21 @@ void margeSort(int arr[], int size){
     int leftHalf[mid];
     int rightHalf[size - mid];
 
-    for(int i = 0; i < mid; i++){
+    for (int i = 0; i < mid; i++) {
         leftHalf[i] = arr[i];
     }
 
-    for(int i = mid; i < size; i++){
+    for (int i = mid; i < size; i++) {
         rightHalf[i - mid] = arr[i];
     }
 
-    margeSort(leftHalf, mid);
-    margeSort(rightHalf, size - mid);
+    mergeSort(leftHalf, mid);
+    mergeSort(rightHalf, size - mid);
 
-    marge(leftHalf, mid, rightHalf, size - mid, arr);
+    merge(leftHalf, mid, rightHalf, size - mid, arr);
 }
 
-int main(){
+int main() {
     int size;
 
     cout << "Enter array size: ";
@@ -63,17 +63,19 @@ int main(){
 
     cout << "Enter elements: ";
 
-    for(int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
 
-    margeSort(arr, size);
+    mergeSort(arr, size);
 
     cout << "Sorted array: ";
 
-    for(int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
+
+    cout << endl;
 
     return 0;
 }
